c98ba5b797d4ad48753e003f2e55cf12f1d7d695
elia0002 <54428692+elia0002@users.noreply.github.com>
2019-10-06 15:22:18 +1100

    placeSearch

---
+++ SoberandSound/Controllers/SportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoberandSound.Controllers
+{
+    public class SportController : Controller
+    {
+        // GET: Sport
+        public ActionResult SportIndex()
+        {
+            return View();
+        }
+    }
+}
